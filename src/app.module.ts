e4953d85b447240016f6b28e0b8138d0663696e0
madelrio <madelrio93@gmail.com>
2021-04-21 12:04:00 -0400

    add graphql module root

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TreatmentFileModule } from './modules/treatment-file/treatment-file.module';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TreatmentFileModule, DatabaseModule],
+  imports: [
+    TreatmentFileModule,
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      context: ({ req }) => ({ req }),
+      autoSchemaFile: join(process.cwd(), `${__dirname}/schema.gql`),
+      sortSchema: true,
+    }),
+  ],
 })
 export class AppModule {}
