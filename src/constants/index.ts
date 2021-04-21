f48d25d79ba0d795f1ddba9a65cb3b445d221e21
madelrio <madelrio93@gmail.com>
2021-04-21 13:20:36 -0400

    add enum status

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,11 @@
 export const TREATMENTS_FILES = 'treatments-files';
 
 export const PATIENTS = 'patients';
+
+
+/**-----------Enums------------------ */
+export enum Status {
+    NO_TRATADO="no tratado",
+    TRATADO="tratado",
+    EN_EQUIPO="en equipo",
+}
