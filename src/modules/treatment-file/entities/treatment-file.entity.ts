dcd24814530b55a1a080b9a25fa876bd3d82a66b
madelrio <madelrio93@gmail.com>
2021-04-21 13:15:37 -0400

    created treatments files entity

--- src/modules/treatment-file/entities/treatment-file.entity.ts
+++ src/modules/treatment-file/entities/treatment-file.entity.ts
@@ -1,10 +1,11 @@
 import { Column, Entity } from 'typeorm';
 import { ObjectType, Field } from '@nestjs/graphql';
 
-import { PrimaryId } from '../../../shared';
+import { PrimaryId } from 'src/shared';
+import { TREATMENTS_FILES } from 'src/constants';
 
 @ObjectType()
-@Entity('treatments-files')
+@Entity(TREATMENTS_FILES)
 export class TreatmentFile extends PrimaryId {
   @Field(() => String)
   @Column('varchar', { name: 'status' })
